91ed41e932c0ed98a3bd1e8b01ece4effd9bb87d
Roma Nifontov <romannif@mail.ru>
2024-12-16 13:06:18 +0500

    промежуточный коммит

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,13 +1,15 @@
 CREATE TABLE account (
                              id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
-                             number bigint NOT NULL UNIQUE,
+                             number VARCHAR(20) NOT NULL UNIQUE,
+                             owner_type VARCHAR(20) NOT NULL,
                              owner_id bigint NOT NULL UNIQUE,
-                             type varchar(64) NOT NULL,
-                             currency varchar(64) NOT NULL,
-                             status varchar(64) NOT NULL,
+                             account_type VARCHAR(30) NOT NULL,
+                             currency CHARACTER(3) NOT NULL,
+                             status VARCHAR(30) NOT NULL,
                              created_at timestamptz DEFAULT current_timestamp,
                              updated_at timestamptz DEFAULT current_timestamp,
-                             close_at timestamptz DEFAULT current_timestamp,
-
-
+                             close_at TIMESTAMP,
+                             version INT NOT NULL
 );
+
+CREATE INDEX idx_number on account(number)
