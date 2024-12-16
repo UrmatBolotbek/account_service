53d5298d3894ac09193e41cc6e24d140f0d8e413
Roma Nifontov <romannif@mail.ru>
2024-12-16 12:31:42 +0500

    промежуточный коммит

--- src/main/resources/db/changelog/changeset/V001__account-service_account.sql
+++ src/main/resources/db/changelog/changeset/V001__account-service_account.sql
@@ -1,1 +1,13 @@
--- Write your sql migration here!
+CREATE TABLE account (
+                             id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY UNIQUE,
+                             number bigint NOT NULL UNIQUE,
+                             owner_id bigint NOT NULL UNIQUE,
+                             type varchar(64) NOT NULL,
+                             currency varchar(64) NOT NULL,
+                             status varchar(64) NOT NULL,
+                             created_at timestamptz DEFAULT current_timestamp,
+                             updated_at timestamptz DEFAULT current_timestamp,
+                             close_at timestamptz DEFAULT current_timestamp,
+
+
+);
