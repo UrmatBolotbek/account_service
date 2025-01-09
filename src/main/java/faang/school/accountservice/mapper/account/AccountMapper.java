b0467ab608e822ce2bc91736bed52b71a048610d
Alena Shmelkova <AlenaShmelkova@gmail.com>
2025-01-09 15:05:41 -0500

    BJS2-41956 SavingsAccount ready without tests

--- src/main/java/faang/school/accountservice/mapper/account/AccountMapper.java
+++ src/main/java/faang/school/accountservice/mapper/account/AccountMapper.java
@@ -4,8 +4,9 @@ import faang.school.accountservice.dto.RequestAccountDto;
 import faang.school.accountservice.dto.ResponseAccountDto;
 import faang.school.accountservice.model.account.Account;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", unmappedSourcePolicy = ReportingPolicy.IGNORE)
 public interface AccountMapper {
 
     Account toAccount(RequestAccountDto account);
