a70f58b44aef9e56d41d0d7efe61de5415d5b74e
IvanMatiko <matiko1993@gmail.com>
2025-01-10 15:50:21 +0100

    BJS2-41952 Правки в тесте. Удаление лишнего импорта

--- src/test/java/faang/school/accountservice/integration/service/payment/PaymentServiceIntegrationTest.java
+++ src/test/java/faang/school/accountservice/integration/service/payment/PaymentServiceIntegrationTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
