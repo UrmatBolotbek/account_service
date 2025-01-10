6bb1eb6500931b0039ebdfd8bbd1351e99d5506c
IvanMatiko <matiko1993@gmail.com>
2025-01-10 01:34:31 +0100

    BJS2-41952 попытка исправить ошибки

--- src/main/java/faang/school/accountservice/config/account_generation/AccountGenerationConfig.java
+++ src/main/java/faang/school/accountservice/config/account_generation/AccountGenerationConfig.java
@@ -4,17 +4,13 @@ import faang.school.accountservice.enums.AccountType;
 import faang.school.accountservice.enums.Currency;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Component
 @ConfigurationProperties(prefix = "scheduler.account.generation")
-@Configuration
-@EnableScheduling
-@Profile("!test")
+@Data
 public class AccountGenerationConfig {
 
     private Map<String, Integer> requiredBatch;
